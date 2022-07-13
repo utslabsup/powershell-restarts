324503bb4740aac6c85d22fbcc6c5f435ab707d5
Daniel A <84752451+danielangeloni@users.noreply.github.com>
2022-07-13 15:26:35 +1000

    Update to use AD to fetch computers

--- ByRoom.ps1
+++ ByRoom.ps1
@@ -2,69 +2,54 @@
 #
 #  Lab Restart Script
 #
-#  author: Andrew Gou
+#  author: Daniel Angeloni
 #
-#  support: sophie.ryan@uts.edu.au & daniel.angeloni@uts.edu.au
+#  support: daniel.angeloni@uts.edu.au
 #
 ###### Set-ExecutionPolicy RemoteSigned -Scope Process
 
-# Machine name text file directory and error log directory, change as needed.
-$fileDir = "Labs\"
-$errorDir = "Errors\"
-$date = Get-Date -Format "dd.MM.yy"
 
-# Create error log file
-If (!(Test-Path -Path "$errorDir$date.txt"))
-{
-    New-Item -ItemType "file" -Path "$errorDir$date.txt" | Out-Null
-}
+while (1) {
+    $lab = Read-Host -Prompt "Input lab location (e.g. 'CB0105000' or 'B0105000')"
 
-Do 
-{
-    # Input lab name
-    while(1) {
-        $lab = Read-Host -Prompt "Input lab location (e.g. 'CB0105000')"
+    # Handle the different potential inputs
+    if ($lab.SubString(0,2) -match "CB") {
+        $lab = $lab.SubString(2)
+    } elseif ($lab[0] -match "B") {
+        $lab = $lab.SubString(1)
+    } else {
+        Write-Host "Invalid format." 
+        continue
+    }
 
-        if (Test-Path -LiteralPath "$fileDir$lab.txt" -PathType Leaf)
-        {
-	    $counter = 0
-	    $successful = 0
-            foreach ($computer in Get-Content -path $fileDir$lab.txt)
-            {
-                # Test connection to each computer
-                if (Test-NetConnection -ComputerName $computer -InformationLevel Quiet)
-                {
-                    # Attempt restart
-                    Restart-Computer -ComputerName $computer -ErrorVariable errmsg -ErrorAction SilentlyContinue
-                        if ($errmsg -ne $null)
-                        {
-                            # Error log: Restart failed
-                            Write-Host "$errmsg" -ForegroundColor red
-                            Add-Content -Path "$errorDir$date.txt" -Value "$errmsg"
-                        }
-                        else
-                        {
-                            # Success
-                            Write-Host "$computer restarted." -ForegroundColor green
-			    $successful++
-                        }
+    # Pull computers from AD (note to self: add in OU for better search functionality and/or searching by room OU)
+    $computers = Get-ADComputer -Filter "Name -like 'LAB$($lab.ToString())*'" | select Name
+    $counter = 0
+    $successful = 0
+
+    foreach ($computer in $computers.Name) {
+        # Test connection to each computer
+        if (Test-NetConnection -ComputerName $computer -InformationLevel Quiet) {
+            # Attempt restart
+            Restart-Computer -ComputerName $computer -ErrorVariable errmsg -ErrorAction SilentlyContinue
+                if ($errmsg -ne $null) {
+                    # Error log: Restart failed
+                    Write-Host "$errmsg" -ForegroundColor red
+                } else {
+                    # Success
+                    Write-Host "$computer restarted." -ForegroundColor green
+                    $successful++
                 }
-                else 
-                {
-                    # Error log: Connection failed
-                    Write-Host "$computer is offline." -ForegroundColor red
-                    Add-Content -Path "$errorDir$date.txt" -Value "$computer is offline."
-                }
-	        $counter++
-            }
+        } else {
+            # Error log: Connection failed
+            Write-Host "$computer is offline." -ForegroundColor red
         }
-        else
-        {
-            # Lab name entered incorrectly
-            Write-Host "Lab does not exist."
-        }
-        Write-Host "Finished for $lab, success rate of $('{0:P2}' -f ($successful / $counter))" 
+        $counter++
+    }
+
+    if ($count -ne 0) {
+        Write-Host "Finished for CB$lab, success rate of $('{0:P2}' -f ($successful / $counter))"
+    } else {
+        Write-Host "Lab does not exist." 
     }
 }
-Until (Test-Path -LiteralPath "$fileDir$lab.txt" -PathType Leaf)
-
