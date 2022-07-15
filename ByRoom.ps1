499f40850526b12ececc1890bffad6e45a30b363
Daniel A <84752451+danielangeloni@users.noreply.github.com>
2022-07-15 12:15:03 +1000

    Update to allow for overrides

--- ByRoom.ps1
+++ ByRoom.ps1
@@ -10,24 +10,34 @@
 
 
 while (1) {
-    $lab = Read-Host -Prompt "Input lab location (e.g. 'CB0105000' or 'B0105000')"
+    $lab = Read-Host -Prompt "Input lab location (e.g. 'CB0105000' or 'B0105000' or '0105000')"
 
     # Handle the different potential inputs
     if ($lab.SubString(0,2) -match "CB") {
         $lab = $lab.SubString(2)
     } elseif ($lab[0] -match "B") {
         $lab = $lab.SubString(1)
+    } elseif ($lab -match "^[0-9]{7}$" -or $lab -match "^[0-9]{4}POD$") {
     } else {
         Write-Host "Invalid format." 
         continue
     }
 
-    # Pull computers from AD (note to self: add in OU for better search functionality and/or searching by room OU)
-    $computers = Get-ADComputer -Filter "Name -like 'LAB$($lab.ToString())*'" | select Name
+    # Manually define certain collections to bypass AD search. This is implemented to allow for aliases such as CB0206POD, which will return no AD objects.
+    if (Test-Path -LiteralPath ".\_overrides\CB$lab.txt" -PathType Leaf) {
+        # Use the override file
+        $computers = Get-Content -Path ".\_overrides\CB$lab.txt"
+    } else {
+        # Pull computers from AD
+        $computers = Get-ADComputer -Filter "Name -like 'LAB$($lab.ToString())*' -or Name -like 'POD$($lab.ToString())*'" | select Name
+        $computers = $computers.Name
+    }
+    
+    # $computers = Get-ADComputer -Filter "Name -like 'LAB$($lab.ToString())*'" | select Name
     $counter = 0
     $successful = 0
 
-    foreach ($computer in $computers.Name) {
+    foreach ($computer in $computers) {
         # Test connection to each computer
         if (Test-NetConnection -ComputerName $computer -InformationLevel Quiet) {
             # Attempt restart
@@ -47,9 +57,9 @@ while (1) {
         $counter++
     }
 
-    if ($count -ne 0) {
+    if ($counter -ne 0) {
         Write-Host "Finished for CB$lab, success rate of $('{0:P2}' -f ($successful / $counter))"
     } else {
-        Write-Host "Lab does not exist." 
+        Write-Host "Lab does not exist (or at least no results returned from AD)"  
     }
 }
